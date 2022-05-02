5236349fac0ab7f05fbe5db1008c57781eaa0d74
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-05-02 17:39:59 +0530

    Update check_decorator_value.py

--- pre_commit_hooks/check_decorator_value.py
+++ pre_commit_hooks/check_decorator_value.py
@@ -2,21 +2,17 @@ from __future__ import annotations
 
 import argparse
 import re
-import io
 import os.path
-import tokenize
-from tokenize import tokenize as tokenize_tokenize
 from typing import Sequence
 
 
 
-def check_decorator(src: bytes, filename: str = '<unknown>') -> int:
+def check_decorator(src: bytes, filename: str = '<unknown>',missing_tag) -> int:
     file = src.readlines()
-    missing_tag = False
+    missing_tag = missing_tag
     lastline = ""
     for line in file:
         if re.search("^class.*:$", line.decode("utf-8")):
-            print(lastline)
             if (
                 lastline == ""
                 or ('funid_test' not in lastline )
@@ -38,17 +34,13 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument('filenames', nargs='*')
     args = parser.parse_args(argv)
     print(args.filenames)
-    retval = 0
+    missing_tag = False
     for filename in args.filenames:
         base = os.path.basename(filename)
-        print("this is base ",base)
-        print(filename)
-        print(re.match("^test.*py$", base))
         if (
                 re.match("^test.*py$", base) and
                 base != '__init__.py' 
         ):
-            print(filename)
             with open(filename, 'rb') as f:
-                retval = check_decorator(f, filename=filename)
-    return True
+                 missing_tag = check_decorator(f, filename=filename,missing_tag)
+    return missing_tag
