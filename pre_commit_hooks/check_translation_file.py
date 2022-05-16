2abf908d416a368d877f014273e5e831a1a9a97c
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-05-16 12:01:38 +0530

    Update check_translation_file.py

--- pre_commit_hooks/check_translation_file.py
+++ pre_commit_hooks/check_translation_file.py
@@ -11,22 +11,18 @@ def searchDuplicates(po_file_path, duplicate,filename):
     """
     Log all those msgid duplicated inside the file
     """
-    with open(filename, 'rb') as f:
-        file = f.read()
-        duplicate = duplicate
-        po_file = po_file_path
-        msg_ids = [entry.msgid for entry in po_file]
-        counter_dict = Counter(msg_ids)
-        if any(el for el in counter_dict.values() if el > 1):
-            print("Duplicates Terms are found!on file ",filename)
-            print('========== Manual Intervention is needed """"""""')
-            duplicate = True
-        for key, value in counter_dict.items():
-            if value > 1:
-                print(key)
-                print(index = file.search(key))
-                
-        return duplicate
+    duplicate = duplicate
+    po_file = po_file_path
+    msg_ids = [entry.msgid for entry in po_file]
+    counter_dict = Counter(msg_ids)
+    if any(el for el in counter_dict.values() if el > 1):
+        print('========== Manual Intervention is needed """"""""')
+        print("[DT8013] Follwing Duplicates Terms are found! on file ",filename)
+        duplicate = True
+    for key, value in counter_dict.items():
+        if value > 1:
+            print(key,",")         
+    return duplicate
 
 
 def sort_entries(po_file_path, duplicate, save=False):
