bdbf914659c224c60cc14716205f89d507f72300
7079 <rahul@sociusus.com>
2022-05-18 09:56:56 +0530

    [FIX][13.0] error

--- pre_commit_hooks/check_translation_file.py
+++ pre_commit_hooks/check_translation_file.py
@@ -23,50 +23,53 @@ def emptypo():
     }
     return po_obj
 
-def search_duplicate_menu(filename,duplicate):
+
+def search_duplicate_menu(filename, duplicate):
     with open(filename, 'rb') as f:
         duplicate = duplicate
         file = f.readlines()
         set_file = set(file)
+        expressions = ("^#: model:ir.ui.menu",
+                       "^#: model:ir.actions.act_window")
         for line in set_file:
-            if re.search("^#: model:ir.ui.menu", line.decode("utf-8")):
-                indices  = [index+1 for (index, item) in enumerate(file) if item == line]
-                if len(indices) >1:
-                    print(
-                        f'[DM8103].'
-                        f'Duplicate Menu on {filename}'
-                        f'(on lines {indices}).',
-                    )
-                    duplicate = True
-            if re.search("^#: model:ir.actions.act_window", line.decode("utf-8")):
-                indices  = [index+1 for (index, item) in enumerate(file) if item == line]
-                if len(indices) >1:
-                    print(
-                        f'[DWA8103].'
-                        f'Duplicate window actions on {filename}'
-                        f'(on lines {indices}).',
-                    )
-                    duplicate = True
-                
-               
-    return duplicate
-                
+            for exp in expressions:
+                if re.search(exp, line.decode("utf-8")):
+                    indices = [index + 1 for (index, item) in enumerate(file)
+                               if item == line]
+                    if len(indices) > 1:
+                        if exp == "^#: model:ir.ui.menu":
+                            print(
+                                f'[DM8103].'
+                                f'Duplicate Menu  on {filename}'
+                                f'(on lines {indices}).',
+                            )
+                        else:
+                            print(
+                                f'[DWA8103].'
+                                f'Duplicate window actions on {filename}'
+                                f'(on lines {indices}).',
+                            )
+                        duplicate = True
 
-def searchDuplicates(po_file_path, duplicate,filename):
+    return duplicate
+
+
+def searchDuplicates(po_file_path, duplicate, filename):
     """
     Log all those msgid duplicated inside the file
     """
     duplicate = duplicate
-    duplicate = search_duplicate_menu(filename,duplicate)
+    duplicate = search_duplicate_menu(filename, duplicate)
     po_file = po_file_path
     msg_ids = [entry.msgid for entry in po_file]
     counter_dict = Counter(msg_ids)
     if any(el for el in counter_dict.values() if el > 1):
-        print("[DT8013] Follwing Duplicates Terms are found! on file ",filename)
+        print("[DT8013] Follwing Duplicates Terms are found! on file ",
+              filename)
         duplicate = True
     for key, value in counter_dict.items():
         if value > 1:
-            print(key,",")         
+            print(key, ",")
     return duplicate
 
 
@@ -86,10 +89,10 @@ def sort_entries(po_file_path, duplicate, save=False):
         file_name = po_file_path
         print("[FS8013] File Sorted", file_name)
         po_out.save(file_name)
-        searchDuplicates(po_sorted,duplicate,po_file_path)
+        searchDuplicates(po_sorted, duplicate, po_file_path)
         return True
-    return searchDuplicates(po_sorted,duplicate,po_file_path)
-    
+    return searchDuplicates(po_sorted, duplicate, po_file_path)
+
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
@@ -98,15 +101,14 @@ def main(argv: Sequence[str] | None = None) -> int:
     duplicate = False
     for filename in args.filenames:
         base = os.path.basename(filename)
-        if (re.search(".po$", base)):
-            duplicate = sort_entries(filename,duplicate,True)
-            
+        if re.search(".po$", base):
+            duplicate = sort_entries(filename, duplicate, True)
+
     return duplicate
 
 
 if __name__ == "__main__":
     raise SystemExit(main())
-
 
 # f = open("/home/socius/fr_CH.po", "r")
 # file = f.read()
