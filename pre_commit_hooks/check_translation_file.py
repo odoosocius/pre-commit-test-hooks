826dca2b1b0b80c3c8b2771587ec30b9b3b36cb5
7079 <rahul@sociusus.com>
2022-05-18 10:24:06 +0530

    [FIX][13.0] remove duplicate msg

--- pre_commit_hooks/check_translation_file.py
+++ pre_commit_hooks/check_translation_file.py
@@ -37,39 +37,12 @@ def search_duplicate_menu(filename, duplicate):
                     indices = [index + 1 for (index, item) in enumerate(file)
                                if item == line]
                     if len(indices) > 1:
-                        if exp == "^#: model:ir.ui.menu":
-                            print(
-                                f'[DM8103].'
-                                f'Duplicate Menu  on {filename}'
-                                f'(on lines {indices}).',
-                            )
-                        else:
-                            print(
-                                f'[DWA8103].'
-                                f'Duplicate window actions on {filename}'
-                                f'(on lines {indices}).',
-                            )
+                        print(
+                            f'[DM8103].'
+                            f'Duplicate {exp} on {filename}'
+                            f'(on lines {indices}).',
+                        )
                         duplicate = True
-
-    return duplicate
-
-
-def searchDuplicates(po_file_path, duplicate, filename):
-    """
-    Log all those msgid duplicated inside the file
-    """
-    duplicate = duplicate
-    duplicate = search_duplicate_menu(filename, duplicate)
-    po_file = po_file_path
-    msg_ids = [entry.msgid for entry in po_file]
-    counter_dict = Counter(msg_ids)
-    if any(el for el in counter_dict.values() if el > 1):
-        print("[DT8013] Follwing Duplicates Terms are found! on file ",
-              filename)
-        duplicate = True
-    for key, value in counter_dict.items():
-        if value > 1:
-            print(key, ",")
     return duplicate
 
 
@@ -77,21 +50,18 @@ def sort_entries(po_file_path, duplicate, save=False):
     """
     Sort a po file
     :rtype: object
+    if sorted will check for duplicates
     """
-    po_sorted = polib.POFile()
     po_file = polib.pofile(po_file_path)
     po_out = emptypo()
-    po_sorted = polib.POFile()
     for entry in sorted(po_file, key=lambda x: x.msgid):
         po_out.append(entry)
-        po_sorted.append(entry)
     if po_out != po_file and save:
         file_name = po_file_path
         print("[FS8013] File Sorted", file_name)
         po_out.save(file_name)
-        searchDuplicates(po_sorted, duplicate, po_file_path)
         return True
-    return searchDuplicates(po_sorted, duplicate, po_file_path)
+    return search_duplicate_menu(po_file_path, duplicate)
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -109,7 +79,3 @@ def main(argv: Sequence[str] | None = None) -> int:
 
 if __name__ == "__main__":
     raise SystemExit(main())
-
-# f = open("/home/socius/fr_CH.po", "r")
-# file = f.read()
-# sortEntries('/home/socius/fr_CH.po',False,True)
