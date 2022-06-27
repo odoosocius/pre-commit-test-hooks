e870622589bb0fee5a893fbe34cad981b4484d55
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-06-27 18:01:09 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -13,7 +13,10 @@ def check_up_to_date():
     directory = os.getcwd()
     print(directory)
     repo = Repo(directory)
+    g=git.cmd.Git(git_dir)
+    print("this is one option " g.fetch())
     print(repo)
+    print("this is  option two" g.fetch('orgin', '--dry-run'))
     result = repo.remote().fetch()
     print(result)
     return True
