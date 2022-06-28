cf651ab75990b6b68315572c211dc02c2857e80b
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-06-28 15:04:01 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -8,7 +8,7 @@ import git
 from git import Repo
 
 
-def check_up_to_date(mis_match):
+def check_up_to_date_with_cloud(mis_match):
     
     directory = os.getcwd()
     print(directory)
@@ -19,9 +19,20 @@ def check_up_to_date(mis_match):
         mis_match = True
     return mis_match
 
+def check_up_to_date_with_local_repo(mis_match):
+    
+    directory = os.getcwd()
+    print(directory)
+    g = git.cmd.Git(directory)
+    result = g.fetch('origin', '--dry-run')
+    print(result,"this is the result")
+    if result:
+        mis_match = True
+    return mis_match
 
 def main():
     mis_match = True
-    mis_match = check_up_to_date(mis_match)
+    mis_match = check_up_to_date_with_cloud(mis_match)
+    mis_match = check_up_to_date_with_local_repo(mis_match)
 
     return mis_match
