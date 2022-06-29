68181484ea2498d49010dd465ff74c1a49a78b26
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-06-29 12:20:36 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -12,30 +12,13 @@ from git import Git
 def check_up_to_date(mis_match):
     
     directory = os.getcwd()
-    print(directory)
     repo = Repo(directory)
-    
-    #print(repo.remote().fetch(),"is empty")
     for data in repo.remote().fetch("--dry-run"):
         print(data.flags)
-        print(data.note)
         print(data.ref)
         print(data.remote_ref_path)
-    #print(repo.remotes.origin.fetch("--dry-run"))
     print(repo.git.rev_list("..main"),"revlist")
-    #print(repo.git.fetch('origin', '--dry-run'))
-    #print(repo.is_dirty(untracked_files=True))
-    for remote in repo.remotes:
-        print(f'- {remote.name} {remote.url}')
-        #print(remote.fetch('--dry-run'),"in loop")
-    gg=Git()
-    #print(gg.fetch('--dry-run'),"is empty")
-    #print(gg.fetch("origin",dry_run=True),"is empty")
-    #print(gg.branch(),"is empty")
-    result=repo.git.fetch('origin', '--dry-run')
-    if result:
-        print("result is true")
-        mis_match = True
+    
     return mis_match
 
 
