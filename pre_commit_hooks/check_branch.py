36053c2dabfdb330b21dfd8fc32eb762b870515c
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-06-30 19:11:18 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -11,7 +11,7 @@ def check_up_to_date(mis_match):
     directory = os.getcwd()
     repo = Repo(directory)
     for data in repo.remote().fetch("--dry-run"):
-        print(str(data.remote_ref_path))
+        print(str(data.remote_ref_path),"thendi")
         print(type(data.remote_ref_path))
         print(str(data.remote_ref_path)=='13.0')
         if data.flags != 4 and str(data.remote_ref_path) == "13.0":
