ae5cbe837fe1de83e6b640117de62ec1838f9ce9
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-06-30 18:19:04 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
-import argparse
-import re
 import os.path
-from typing import Sequence
 import git
 from git import Repo
 from git import Git
@@ -14,17 +11,18 @@ def check_up_to_date(mis_match):
     directory = os.getcwd()
     repo = Repo(directory)
     for data in repo.remote().fetch("--dry-run"):
-        if data.flags!=4 and data.remote_ref_path =="main":
+        if data.flags != 4 and data.remote_ref_path == "13.0":
             mis_match = True
             print(
                     f'[FD813].'
-                    f'branch  is not up to date .'  
+                    f'Your local repository is not up'
+                    f'to date with production repository'
                 )
-    if repo.git.rev_list("..main"):
+    if repo.git.rev_list("..13.0"):
         mis_match = True
         print(
                     f'[FD813].'
-                    f'branch is not up to date .'  
+                    f'Your branch is not up to date with origin/13.0'
                 )
     return mis_match
 
@@ -32,5 +30,4 @@ def check_up_to_date(mis_match):
 def main():
     mis_match = False
     mis_match = check_up_to_date(mis_match)
-
-    return mis_match
+    return 
