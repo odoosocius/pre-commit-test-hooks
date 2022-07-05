aa17d8bdc7da4d6005f3391b70d6c2dd320c1c3a
odoosocius <31440089+odoosocius@users.noreply.github.com>
2022-07-05 11:48:50 +0530

    Update check_branch.py

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -5,6 +5,7 @@ import git
 from git import Repo
 from git import Git
 
+
 def check_remote(mis_match):
     directory = os.getcwd()
     repo = Repo(directory)
@@ -13,48 +14,48 @@ def check_remote(mis_match):
         g.ls_remote('upstream').split('\n')
     except Exception:
         print(
-                    f'[AUR813].'
-                    f'You seem not to have an upstream remote'
-                )
+            f'[AUR813].'
+            f'You seem not to have an upstream remote'
+        )
         mis_match = True
-        return mis_match  
-    print(g.remote('get-url','origin'))
-    if( g.remote('get-url','origin') == 
-       'https://github.com/odoosocius/TestCodeRepo.git'):
-         print(
-                    f'[FOR813].'
-                    f'Remote origin is pointing to FernUni'
-                    f'repository and should be linked to your Fork url'
-                )
-        mis_match = True
-    if( g.remote('get-url','upstream') != 
-       'https://github.com/odoosocius/TestCodeRepo.git'):
+        return mis_match
+    print(g.remote('get-url', 'origin'))
+    if (g.remote('get-url', 'origin') ==
+            'https://github.com/odoosocius/TestCodeRepo.git'):
         print(
-                    f'[FUS813].'
-                    f'Your upstream remote is'
-                    f'not the FernUni repository'
-                )
+            f'[FOR813].'
+            f'Remote origin is pointing to FernUni'
+            f'repository and should be linked to your Fork url'
+        )
         mis_match = True
-    return mis_match   
-    
+    if (g.remote('get-url', 'upstream') !=
+            'https://github.com/odoosocius/TestCodeRepo.git'):
+        print(
+            f'[FUS813].'
+            f'Your upstream remote is'
+            f'not the FernUni repository'
+        )
+        mis_match = True
+    return mis_match
+
+
 def check_up_to_date(mis_match):
-    
     directory = os.getcwd()
     repo = Repo(directory)
     for data in repo.remote('upstream').fetch("--dry-run"):
         if data.flags != 4 and (data.remote_ref_path).strip() == "main":
             mis_match = True
             print(
-                    f'[FD813].'
-                    f'Your local repository is not up'
-                    f'to date with production repository'
-                )
+                f'[FD813].'
+                f'Your local repository is not up'
+                f'to date with production repository'
+            )
     if repo.git.rev_list("..remotes/upstream/main"):
         mis_match = True
         print(
-                    f'[FD813].'
-                    f'Your branch is not up to date with upstream/13.0'
-                )
+            f'[FD813].'
+            f'Your branch is not up to date with upstream/13.0'
+        )
     return mis_match
 
 
