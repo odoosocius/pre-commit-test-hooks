7fe0291d6656fa4e6c9a180ef16ab2a3d1375a59
7079 <rahul@sociusus.com>
2022-07-12 11:25:05 +0530

    [ADD][15.0] Test for FSCH Customer

--- pre_commit_hooks/check_migration_tools.py
+++ pre_commit_hooks/check_migration_tools.py
@@ -5,7 +5,9 @@ import re
 import os.path
 from typing import Sequence
 
-def check_migration_folder(condition_failed):
+def check_migration_folder(dir_list,condition_failed):
+    for directory in dir_list:
+        print(directory)
     return condition_failed
 
 
@@ -13,21 +15,27 @@ def check_migration_folder(condition_failed):
 
 def main(argv: Sequence[str] | None = None) -> int:
     condition_failed = True
-    condition_failed = check_migration_folder(condition_failed)
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('filenames', nargs='*')
-    # args = parser.parse_args(argv)
-    # missing_tag = False
-    # for filename in args.filenames:
-    #     dir = os.path.dirname(filename).split('/')
-    #     if '' in dir:
-    #         base = os.path.basename(filename)
-    #         if (
-    #                 re.match("^test.*py$", base) or
-    #                 base == 'common.py'
-    #         ):
-    #             with open(filename, 'rb') as f:
-    #                 missing_tag = check_decorator(
-    #                     f, missing_tag, filename=filename
-    #                 )
+    # condition_failed = check_migration_folder(condition_failed)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+    dir_list = []
+    for filename in args.filenames:
+        dir = os.path.dirname(filename).split('/')
+        dir_list.append(dir)
+    print(dir_list)
+    dir_list  = list(set(dir_list))
+    condition_failed = check_migration_folder(dir_list,condition_failed)
+
+
+        # if '' in dir:
+        #     base = os.path.basename(filename)
+        #     if (
+        #             re.match("^test.*py$", base) or
+        #             base == 'common.py'
+        #     ):
+        #         with open(filename, 'rb') as f:
+        #             missing_tag = check_decorator(
+        #                 f, missing_tag, filename=filename
+        #             )
     return condition_failed
