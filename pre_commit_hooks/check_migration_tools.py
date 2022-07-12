1ccf941f0c0c732d42d6e8e95f108f4fb8d8d89e
7079 <rahul@sociusus.com>
2022-07-12 10:48:04 +0530

    [ADD][main]add precommit

---
+++ pre_commit_hooks/check_migration_tools.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import argparse
+import re
+import os.path
+from typing import Sequence
+
+def check_migration_folder(condition_failed):
+    return condition_failed
+
+
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    condition_failed = True
+    condition_failed = check_migration_folder(condition_failed)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('filenames', nargs='*')
+    # args = parser.parse_args(argv)
+    # missing_tag = False
+    # for filename in args.filenames:
+    #     dir = os.path.dirname(filename).split('/')
+    #     if '' in dir:
+    #         base = os.path.basename(filename)
+    #         if (
+    #                 re.match("^test.*py$", base) or
+    #                 base == 'common.py'
+    #         ):
+    #             with open(filename, 'rb') as f:
+    #                 missing_tag = check_decorator(
+    #                     f, missing_tag, filename=filename
+    #                 )
+    return condition_failed
